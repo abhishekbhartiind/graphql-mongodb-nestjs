d8cb6900f22bac39e0ecd11c22541c18f1849231
abhishekbhartiind <imabhishekbharti12@gmail.com>
2023-06-25 12:42:17 +0530

    nextjs, graphql, mongodb

---
+++ src/books/book.input.ts
@@ -0,0 +1,14 @@
+// book.input.ts
+import { InputType, Field } from '@nestjs/graphql';
+
+@InputType()
+export class BookInput {
+  @Field()
+  title: string;
+
+  @Field()
+  author: string;
+
+  @Field()
+  publishedDate: boolean;
+}
